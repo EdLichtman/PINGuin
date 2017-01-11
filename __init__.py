cd426fc4a5cdcfc875494c2d740d524a74987c5a
Edward Lichtman <E.Lichtman2@gmail.com>
2017-01-11 08:22:28 -0500

    refactor and accept json_standard_headers
    refactor build_request into its own function
    
    Allow for new JSON Property: json_standard_headers for better syntax

--- __init__.py
+++ __init__.py
@@ -10,6 +10,41 @@ def find_targets_to_ping_from_file():
     url_targets = open("/".join([home_directory,  ping_file]), "r").readlines()
     return  json.loads("".join(url_targets))
 
+def attr_is_none_type(request_object, attr):
+    return ((attr in request_object and request_object[attr] is None)
+            or attr not in request_object)
+
+def build_request(ping_object):
+
+    request_object = {}
+
+    request_object["url"] = ping_object["url"]
+
+    if ('body' in ping_object):
+        request_object["body"] = json.loads(ping_object["body"])
+    else:
+        request_object["body"] = None
+
+    if ('headers' in ping_object):
+        headers = json.loads(ping_object["headers"])
+
+        request_object["headers"] = headers
+    else:
+        request_object["headers"] = None
+
+    if ('json_standard_headers' in ping_object):
+        if (attr_is_none_type(request_object, 'headers')):
+                request_object["headers"] = {}
+
+
+        json_standard_headers = ping_object["json_standard_headers"]
+
+        for header in json_standard_headers:
+            request_object["headers"][header] = json_standard_headers[header]
+
+    return request_object
+
+
 def package_ping(request, my_api_response):
     if (request.status_code == 200):
         my_api_response.append_successful_pings(package_successful_ping(request))
@@ -47,27 +82,19 @@ def api_root():
     response_object = api_response()
     list_of_targets_to_ping = find_targets_to_ping_from_file()
     for target_to_ping in list_of_targets_to_ping["urls"]:
-        url = target_to_ping["url"]
 
-        if ('body' in target_to_ping):
-            body = json.loads(target_to_ping["body"])
-        else: body = None
-
-        if ('headers' in target_to_ping):
-            headers = json.loads(target_to_ping["headers"])
-        else: headers = None
+        request_info = build_request(target_to_ping)
 
         if(target_to_ping["method"] == "GET"):
-            request = requests.get(url, headers = headers)
+            request = requests.get(request_info["url"],
+                                   headers = request_info["headers"])
 
             package_ping(request, response_object)
 
         if(target_to_ping["method"] == "POST"):
-            if ("body" in vars()):
-                data = body
-            else:
-                data = None
-            request = requests.post(url,data, headers = headers)
+            request = requests.post(request_info["url"],
+                                    request_info["body"],
+                                    headers = request_info["headers"])
 
             package_ping(request, response_object)
 
