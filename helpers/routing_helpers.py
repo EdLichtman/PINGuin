ad5cd65e4e0dd589f60f063052f1aeecd2c8882f
EdLichtman <E.Lichtman2@gmail.com>
2017-03-15 21:46:39 -0400

    Dump Json object instead of JSON string

--- helpers/routing_helpers.py
+++ helpers/routing_helpers.py
@@ -1,4 +1,3 @@
-from flask import jsonify
 from helpers.request_helpers import make_request, build_request
 from models.models import ApiResponse
 
@@ -8,8 +7,7 @@ def ping_all_target_urls(list_of_targets_to_ping):
     response_object = ApiResponse()
     for target_to_ping in list_of_targets_to_ping["urls"]:
         request_info = build_request(target_to_ping)
-
         request = make_request(request_info)
-
         response_object.appendPings(request)
-    return jsonify(response_object.getJSON())
+
+    return response_object.getJSON()
--- models/models.py
+++ models/models.py
@@ -1,20 +1,23 @@
-import json, base64
+import json
+from flask import Response
 
 class ApiResponse():
 
     def appendPings(self, ping):
         request = ping.request
-        response = { "request" : { "url" : request.url
-                                   , "method" : request.method
-                                   , "headers" : dict(request.headers)
-                                   , "body" : request.body}
-                    , "status_code" : ping.status_code
-                    , "headers" : dict(ping.headers)
-                    , "content" : ping.text }
+        response = { 'request' : { 'url' : request.url
+                                   , 'method' : request.method
+                                   , 'headers' : dict(request.headers)
+                                   , 'body' : request.body}
+                    , 'status_code' : ping.status_code
+                    , 'headers' : dict(ping.headers) }
         self.pings.append(response)
 
     def getJSON(self):
-        return json.dumps({ "result" : self.pings})
+        resp = Response(response= json.dumps({ 'result' : self.pings}),
+                        status=200,
+                        mimetype="application/json")
+        return resp
 
     pings = []
 
