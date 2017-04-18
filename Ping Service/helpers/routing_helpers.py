cebbd295bf89b3e50dbf13a68200bb9facc6ebb8
EdLichtman <E.Lichtman2@gmail.com>
2017-04-18 17:30:41 -0400

    Moves PINGuin into a sub-service folder

--- Ping Service/helpers/routing_helpers.py
+++ Ping Service/helpers/routing_helpers.py
@@ -1,5 +1,6 @@
 from helpers.request_helpers import make_request, build_request
 from models.models import ApiResponse
+from flask import abort
 
 
 
@@ -8,6 +9,9 @@ def ping_all_target_urls(list_of_targets_to_ping):
     for target_to_ping in list_of_targets_to_ping["urls"]:
         request_info = build_request(target_to_ping)
         request = make_request(request_info)
-        response_object.appendPings(request)
+        if (hasattr(request, "request")):
+            response_object.appendPings(request)
+        else:
+            return abort(500, "Request was undeliverable")
 
     return response_object.getJSON()
